317a719218de8faf50376b2d74b355a2f3b03e9b
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-07-27 05:03:51 +0300

    build: configure eas and use env

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+import { ConfigContext, ExpoConfig } from "@expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: process.env.EXPO_PUBLIC_PROD_NAME,
+  slug: process.env.EXPO_PUBLIC_PROD_SLUG,
+  version: process.env.EXPO_PUBLIC_VERSION,
+  description: process.env.EXPO_PUBLIC_PROD_DESCRIPTION,
+  plugins: [
+    [
+      "@rnmapbox/maps",
+      {
+        RNMapboxMapsImpl: "mapbox",
+        RNMapboxMapsDownloadToken: process.env.RNMAPBOX_MAPS_DOWNLOAD_TOKEN,
+      },
+    ],
+    [
+      "expo-location",
+      {
+        locationWhenInUsePermission: `Authorisé ${process.env.EXPO_PUBLIC_PROD_NAME} à accéder à votre position ?`,
+      },
+    ],
+  ],
+});
