01b8d04ba8e64466c8e28200013e45d092448233
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-08-22 09:48:02 +0300

    build: add expo-router plugin

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,13 @@ import { ConfigContext, ExpoConfig } from "@expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  scheme: "zahatany",
   name: process.env.EXPO_PUBLIC_PROD_NAME,
   slug: process.env.EXPO_PUBLIC_PROD_SLUG,
   version: process.env.EXPO_PUBLIC_VERSION,
   description: process.env.EXPO_PUBLIC_PROD_DESCRIPTION,
   plugins: [
+    "expo-router",
     [
       "@rnmapbox/maps",
       {
